7e7fded07de837f8301d8b01e062469b115f36a4
Kritika Goyal <kritikagoyal@adobe.com>
2024-12-11 14:49:25 +0530

    Update test.js

--- tools/sidekick/test.js
+++ tools/sidekick/test.js
@@ -7,11 +7,4 @@ const sk = document.querySelector('aem-sidekick');
 if (sk) {
   // sidekick already loaded
     sk.addEventListener('custom:foo', doFoo);
-} else {
-  // wait for sidekick to be loaded
-  document.addEventListener('sidekick-ready', () => {
-    // sidekick now loaded
-    document.querySelector('aem-sidekick')
-      .addEventListener('custom:foo', doFoo);
-  }, { once: true });
 }
