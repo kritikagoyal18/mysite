c7525fda0684c4fcd508b053d0fa455bc4dd32dc
Kritika Goyal <kritikagoyal@adobe.com>
2024-12-12 14:43:35 +0530

    Update script.js

--- tools/sidekick/script.js
+++ tools/sidekick/script.js
@@ -1,7 +1,22 @@
+import { sp } from "@pnp/sp";
+
+async function fetchFolderHierarchy() {
+  try {
+    const libraryPath = 'https://drive.google.com/drive/folders/1I5OPtzRgSRdRE9qEG2sXeownbAkwUJbO';
+    console.log('url is ', libraryPath);
+    const folders = await sp.web.getFolderByServerRelativeUrl(libraryPath).folders.get();
+    console.log('folders are ', folders);
+    return folders.map((folder) => ({ name: folder.Name, url: folder.ServerRelativeUrl }));
+  } catch (error) {
+    console.error("Error fetching folder hierarchy:", error);
+    throw new Error("Unable to fetch folders.");
+  }
+}
+
 function renderFolderStructure() {
   const contentUrl = 'https://drive.google.com/drive/folders/1I5OPtzRgSRdRE9qEG2sXeownbAkwUJbO';
   console.log('url is ', contentUrl);
   // Logic to load the folder structure from this URL
 }
 
-renderFolderStructure();
+fetchFolderHierarchy();
