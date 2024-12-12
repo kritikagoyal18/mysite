5bf7084acbe558794c8c7bcebd5157fb9045cbe1
Kritika Goyal <kritikagoyal@adobe.com>
2024-12-12 12:46:10 +0530

    Update script.js

--- tools/sidekick/script.js
+++ tools/sidekick/script.js
@@ -1,7 +1,7 @@
 function renderFolderStructure() {
-  const spUrl = 'https://drive.google.com/drive/folders/1I5OPtzRgSRdRE9qEG2sXeownbAkwUJbO';
-  console.log('url is ', spUrl);
-  // Logic to load the folder structure of this URL
+  const contentUrl = 'https://drive.google.com/drive/folders/1I5OPtzRgSRdRE9qEG2sXeownbAkwUJbO';
+  console.log('url is ', contentUrl);
+  // Logic to load the folder structure from this URL
 }
 
 renderFolderStructure();
