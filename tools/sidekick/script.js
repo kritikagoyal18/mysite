d1271d5993257741b397b63b9800607f3412f088
Kritika Goyal <kritikagoyal@adobe.com>
2024-12-13 07:19:54 +0530

    reset

--- tools/sidekick/script.js
+++ tools/sidekick/script.js
@@ -1,20 +1,7 @@
-async function fetchFolderHierarchy() {
-  try {
-    const libraryPath = 'https://drive.google.com/drive/folders/1I5OPtzRgSRdRE9qEG2sXeownbAkwUJbO';
-    console.log('url is ', libraryPath);
-    const folders = await sp.web.getFolderByServerRelativeUrl(libraryPath).folders.get();
-    console.log('folders are ', folders);
-    return folders.map((folder) => ({ name: folder.Name, url: folder.ServerRelativeUrl }));
-  } catch (error) {
-    console.error("Error fetching folder hierarchy:", error);
-    throw new Error("Unable to fetch folders.");
-  }
-}
-
 function renderFolderStructure() {
   const contentUrl = 'https://drive.google.com/drive/folders/1I5OPtzRgSRdRE9qEG2sXeownbAkwUJbO';
   console.log('url is ', contentUrl);
   // Logic to load the folder structure from this URL
 }
 
-fetchFolderHierarchy();
+renderFolderStructure();
